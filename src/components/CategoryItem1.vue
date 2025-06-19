ab059e8672ba938ebaab3780134c470f8dfce4f1
hamdanmaulanamuhammad <160691851+hamdanmaulanamuhammad@users.noreply.github.com>
2025-06-19 17:00:32 +0700

    seller view, category view

--- src/components/CategoryItem1.vue
+++ src/components/CategoryItem1.vue
@@ -1,19 +1,26 @@
 <template>
-    <div class="flex flex-col items-center">
-      <a :href="link">
-        <img :src="image" :alt="'Icon for ' + name" class="mb-2 w-8 h-8 sm:w-12 sm:h-12 lg:w-18 lg:h-18">
-        <span class="text-xs sm:text-base lg:text-md">{{ name }}</span>
-      </a>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      name: String,
-      image: String,
-      link: String
-    }
-  };
-  </script>
-  
+  <div
+    class="flex flex-col items-center justify-center w-full h-24 sm:h-28 lg:h-32 p-2 hover:bg-gray-100 rounded-lg transition"
+  >
+    <a :href="link" class="flex flex-col items-center justify-center w-full h-full">
+      <img
+        :src="image"
+        :alt="'Icon for ' + name"
+        class="w-12 h-12 sm:w-14 sm:h-14 lg:w-16 lg:h-16 object-contain mb-2"
+      />
+      <span class="text-xs sm:text-sm lg:text-base text-center leading-tight line-clamp-2">
+        {{ name }}
+      </span>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    image: String,
+    link: String,
+  },
+}
+</script>
---
+++ src/components/ProductCard1.vue
@@ -0,0 +1,54 @@
+```vue
+<template>
+  <div
+    class="p-4 mx-auto border border-gray-200 shadow-md rounded-lg hover:shadow-lg transition min-h-[350px]"
+  >
+    <router-link :to="link" class="block">
+      <img
+        :src="image"
+        :alt="'Product image for ' + name"
+        class="w-full h-[300px] object-cover mb-2 rounded-t-lg"
+      />
+      <div class="p-2">
+        <div class="text-sm lg:text-base font-semibold truncate">{{ name }}</div>
+        <div class="text-gray-500 text-sm lg:text-base">Rp{{ price }}</div>
+      </div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: [String, Number],
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    link: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped>
+a,
+.router-link-active {
+  text-decoration: none;
+  color: inherit;
+}
+</style>
+```
